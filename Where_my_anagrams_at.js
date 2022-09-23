08fff90b58cca0ccc966bcf00d5792e8308d65b1
zvizzct <victor.puerta01@estudiant.upf.edu>
2022-09-23 12:21:29 +0200

    Where my anagrams at?, day 5

---
+++ Where_my_anagrams_at.js
@@ -0,0 +1,16 @@
+//Write a function that will find all the anagrams of a word from a list.
+//You will be given two inputs a word and an array with words. You should
+//return an array of all the anagrams or an empty array if there are none.
+//For example:
+//anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+//anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+function anagrams(word, words) {
+  let arr = [];
+  words.forEach((element) => {
+    if (word.split("").sort().join("") === element.split("").sort().join(""))
+      arr.push(element);
+  });
+  return arr;
+}
+console.log(anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"]));
+// --> [ 'aabb', 'bbaa' ]
